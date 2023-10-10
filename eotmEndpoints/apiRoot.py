aa5332ee9ddfcaad4de7cc0ed583f2ab9b6c0bbd
Moetasim Rady <moetasim.rady@itworx.com>
2023-10-11 01:14:56 +0300

    minor change

--- eotmEndpoints/apiRoot.py
+++ eotmEndpoints/apiRoot.py
@@ -130,6 +130,6 @@ async def resetting_password(new_user_data:User):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8001)
+    uvicorn.run(app, host="127.0.0.1")
 
 
